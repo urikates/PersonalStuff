3abe7f57037f57bb6e9c438936ecb654c9edfdd8
urikates <uvr3141@gmail.com>
2024-07-02 23:00:58 -0600

    Refactor of ImportCFDI, using different libraries

--- ImpCFDI.py
+++ ImpCFDI.py
@@ -5,44 +5,45 @@
 ###########  NOMBRE O RAZON SOCIAL (EMISOR)
 ###########  TOTAL
 ###########  EFECTO DEL COMPROBANTE
+###########  Paste this information on xlsx file  
 ######################################################################
 
 
 import tkinter as tk
-from tkinter import filedialog
-from unittest import skip
-from xml.dom.xmlbuilder import Options
-import pandas as pds
-import pdfquery as pdfq
-
-
-
-#tk.filedialog.SaveAs(master=None, )
-def startprocess():
-    ### PDF Convertion 
-    pdf=pdfq.PDFQuery('CADL-JUN21-G.pdf')
-    pdf.load()
-    pdf.tree.write('CADL-JUN21-G.xml', pretty_print=True)
-    pdf
-    
-    ###Extracting Information
-    pages = pdf.doc.catalog['Pages'].resolve()['Count']
-    pagenumber=pdf.doc.catalog['Pages'].resolve()['Count']
-    while pagenumber < pages:
-            skip ##Just to avoid errors 
+import PyPDF2
            
+openpdf = open('CADL-JUN21-G.pdf', 'rb')
+pdffile = PyPDF2.PdfReader(openpdf)
+# pdffile.getNumPages()
+page = pdffile.pages[0]
+# print(page.extract_text())
+
+parts = []
+def visitor_body(text, cm, tm, fontdict, fontsize):
+    y=tm[5]
+    if y>10:
+        parts.append(text)
+
+page.extract_text(visitor_text=visitor_body)
+text ="".join(parts)
+
+print(text)
+
+
 
 
 ### GUI
-w=tk.Tk()
-w.geometry('800x400')
-w.title('CFDI to Excel')
-w.config(bg='Snow')
-pdflabel=tk.Label(w, text= 'Seleccionar pdf', font=('Georgia Bold',20), bg='White', fg='Black').pack()
-close=tk.Button(w, text='Salir', command=w.destroy, bg='White', fg='Black', font='Georgia').place(x=25, y=350)
-start=tk.Button(w, text='Iniciar', command=startprocess, bg='White', fg='Black', font='Georgia').place(x=725, y=350)
+# w=tk.Tk()
+# w.geometry('800x400')
+# w.title('CFDI to Excel')
+# w.config(bg='Snow')
+# pdflabel=tk.Label(w, text= 'Seleccionar pdf', font=('Georgia Bold',20), bg='White', fg='Black').pack()
+# close=tk.Button(w, text='Salir', command=w.destroy, bg='White', fg='Black', font='Georgia').place(x=25, y=350)
+# start=tk.Button(w, text='Iniciar', command=startprocess, bg='White', fg='Black', font='Georgia').place(x=725, y=350)
+# selectfile= tk.Button(w, text='Seleccionar archivo', command=savefile, bg='white', fg='black', font='Georgia').place(x=300,y=200)
+# pdfsellabel= tk.Label(w, text='PDF no seleccionado', font=('Georgia Bold',15), bg='White', fg='Black'). place(x=25,y=100)
 
-w.mainloop()
+#w.mainloop()
 
 
 
